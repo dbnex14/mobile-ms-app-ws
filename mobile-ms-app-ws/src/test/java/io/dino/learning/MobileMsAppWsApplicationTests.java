15389a3eaa011285ffd1ddc1805e5b01e28ff672
dino <mypascal2000@gmail.com>
2022-05-05 08:30:09 -0700

    Initial commit of project scaffold for spring boot microservices with spring cloud.

---
+++ mobile-ms-app-ws/src/test/java/io/dino/learning/MobileMsAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dino.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileMsAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
