997a91757e504e3aa84ed6c2a069143e4e7e3651
dino <mypascal2000@gmail.com>
2022-05-05 09:06:32 -0700

    added logic to get user by id.

--- mobile-ms-app-ws/src/main/java/io/dino/learning/controller/UserController.java
+++ mobile-ms-app-ws/src/main/java/io/dino/learning/controller/UserController.java
@@ -2,6 +2,7 @@ package io.dino.learning.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")  //http://localhost:8080/users
 public class UserController {
 
-	@GetMapping
-	public String getUser() {
-		return "getUser was called";
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "getUser was called with userId = " + userId;
 	}
 	
 	@PostMapping
